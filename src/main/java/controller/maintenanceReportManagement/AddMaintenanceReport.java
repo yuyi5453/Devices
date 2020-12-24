8794d939d8c6d276c2076b5679818df71f63bc12
yuyi5453 <744530689@qq.com>
2020-12-24 19:10:50 +0800

    add maintenance report

--- src/main/java/controller/maintenanceReportManagement/AddMaintenanceReport.java
+++ src/main/java/controller/maintenanceReportManagement/AddMaintenanceReport.java
@@ -1,4 +1,25 @@
 package controller.maintenanceReportManagement;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pojo.RepairRecord;
+import service.RepairRecordService;
+import util.json.RestResult;
+import util.json.ResultCode;
+
+import java.util.Date;
+
+@RestController
 public class AddMaintenanceReport {
+    @Autowired
+    private RepairRecordService repairRecordService;
+
+    @RequestMapping("/addMaintenanceReport")
+    public String add(RepairRecord record){
+        record.setStatus(RepairRecord.INPROCESS);
+        record.setRepairTime(new Date());
+        repairRecordService.save(record);
+        return new RestResult().setCode(ResultCode.SUCCESS).setMessage("成功").toString();
+    }
 }
--- src/main/java/controller/maintenanceReportManagement/PostConfirmationForm.java
+++ src/main/java/controller/maintenanceReportManagement/PostConfirmationForm.java
@@ -1,4 +1,9 @@
 package controller.maintenanceReportManagement;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PostConfirmationForm {
+
+
 }
