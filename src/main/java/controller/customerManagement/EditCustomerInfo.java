7d1c7bdb03422295e1d234d6adda976a8e6c1f40
yuyi5453 <744530689@qq.com>
2020-12-28 22:08:31 +0800

    用户界面前后端连接

--- src/main/java/controller/customerManagement/EditCustomerInfo.java
+++ src/main/java/controller/customerManagement/EditCustomerInfo.java
@@ -3,9 +3,11 @@ package controller.customerManagement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import pojo.Customer;
 import service.CustomerService;
 
+@RestController
 public class EditCustomerInfo {
     @Autowired
     private CustomerService customerService;
